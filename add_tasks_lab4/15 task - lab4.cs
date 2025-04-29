dbe15401cf413e8b7512ad5b7112dcb61481caa2
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2025-04-29 12:25:52 +0300

    Adding 15 task from 4 lab

---
+++ add_tasks_lab4/15 task - lab4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace task15_lab4
+{
+    internal class Program
+    {
+        static bool CheckedBrackets(string input)
+        {
+            int balance = 0;
+
+            foreach (char c in input)
+            {
+                if (c == '(')
+                {
+                    balance++;
+                }
+                else if (c == ')')
+                {
+                    balance--;
+                    if (balance < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return balance == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            string input = Console.ReadLine();
+
+            if (CheckedBrackets(input))
+            {
+                Console.WriteLine("Розстановка дужок правильна");
+            }
+            else
+            {
+                Console.WriteLine("Розстановка дужок неправильна");
+            }
+        }
+    }
+}
